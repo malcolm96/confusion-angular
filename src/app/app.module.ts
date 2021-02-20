ee832349ceda4849f1e7334daa7e61e3811f3faa
Arfan Shaikh <arfanshaikh1@gmail.com>
2021-02-20 16:20:26 +0530

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -45,7 +46,11 @@ import { LoginComponent } from './login/login.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatDialogModule,
     AppRoutingModule
+  ],
+  entryComponents:[
+    LoginComponent
   ],
   providers: [
     DishService, PromotionService,LeaderService
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog , MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
